9f9134c70ec2723fe4c7aee5e4cf0bc60398b0d4
CoderGamer123 <blooketa50@gmail.com>
2026-01-04 19:04:25 -0500

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,8 +1,7 @@
 // helper.js
-
 function getCurrentUser(){
   let username = localStorage.getItem("currentUser");
-  if(!username) {
+  if(!username){
     alert("You must be logged in!");
     window.location.href = "signin.html";
     return null;
@@ -32,7 +31,6 @@ function saveUserData(userData){
   users[username] = userData;
   localStorage.setItem("users", JSON.stringify(users));
 
-  // Also update current game state for pages
   localStorage.setItem("coins", userData.coins);
   localStorage.setItem("characters", JSON.stringify(userData.characters));
   localStorage.setItem("packsOpened", userData.packsOpened);
